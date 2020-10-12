34bb13b5a864f034842cdd6af2156536b2a3b5a3
cp <dubey.rounak@gmail.com>
2020-10-12 18:13:14 +0530

    Initial commit

---
+++ test/login_test.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+import pytest
+import allure
+from pages.loginpage import LoginPage
+from pages.homepage import HomePage
+from utils import utils as utils
+import moment
+
+
+@pytest.mark.usefixtures("test_setup")
+class Test_Login():
+
+    def test_login(self):
+        driver = self.driver
+        #driver.get("https://opensource-demo.orangehrmlive.com")
+        driver.get(utils.URL)
+        login = LoginPage(driver)
+        #login.enter_username("Admin")
+        login.enter_username(utils.USERNAME)
+        #login.enter_password("admin123")
+        login.enter_password(utils.PASSWORD)
+        login.clickonLogin()
+
+        # driver.find_element_by_id("txtUsername").send_keys("Admin")
+        # driver.find_element_by_id("txtPassword").send_keys("admin123")
+        # driver.find_element_by_id("btnLogin").click()
+
+    def test_logout(self):
+        try:
+            driver = self.driver
+            logout = HomePage(driver)
+            logout.click_Welcome()
+            logout.click_Logout()
+            x = driver.title
+            assert x == "Orange"
+
+        except AssertionError as error:
+            print("There is an error")
+            print(error)
+            currTime= moment.now().strftime("%d-%m-%Y_%H-%M-%S")
+            testName_ScreenshotName = utils.whomami()
+            screenshotname = testName_ScreenshotName + "__" + "ScreenshotName"+ "_" +currTime
+            allure.attach(self.driver.get_screenshot_as_png(),name=screenshotname,
+                          attachment_type=allure.attachment_type.PNG)
+            raise
+
+        except:
+            print("Exception is handling here")
+
+        # driver.find_element_by_id("welcome").click()
+        # driver.find_element_by_link_text("Logout").click()
+
+    #(In above, we have used "assert" statement, therfore Assertion Error we are catching
+    # #Other than "Assertion" any error occur we can catch in except block )
+
---
+++ test/test_firstnormal_login.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import pytest
+from pages.loginpage import LoginPage
+from pages.homepage import HomePage
+from utils import utils as utils
+
+# It uses homepage, utils, loginpage action.
+
+class Test_Login():
+    @pytest.fixture(scope="class")
+    def test_setup(self):
+        global driver
+        driver = webdriver.Chrome(executable_path="C:/Users/cp/PycharmProjects/EntireTask/drivers/chromedriver.exe")
+        driver.implicitly_wait(5)
+        driver.maximize_window()
+        yield
+        driver.close()
+        driver.quit()
+        print("Test Done")
+
+    def test_login(self,test_setup):
+        #driver.get("https://opensource-demo.orangehrmlive.com")
+        driver.get(utils.URL)
+        login = LoginPage(driver)
+        #login.enter_username("Admin")
+        login.enter_username(utils.USERNAME)
+        #login.enter_password("admin123")
+        login.enter_password(utils.PASSWORD)
+        login.clickonLogin()
+
+        # driver.find_element_by_id("txtUsername").send_keys("Admin")
+        # driver.find_element_by_id("txtPassword").send_keys("admin123")
+        # driver.find_element_by_id("btnLogin").click()
+
+    def test_logout(self,test_setup):
+        logout = HomePage(driver)
+        logout.click_Welcome()
+        logout.click_Logou()
+
+        # driver.find_element_by_id("welcome").click()
+        # driver.find_element_by_link_text("Logout").click()
+
+
